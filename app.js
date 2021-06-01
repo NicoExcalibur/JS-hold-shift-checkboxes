b49d7ae076b734322a112984c94e64649bef8079
NicoExcalibur <nico.zanz@gmail.com>
2021-06-01 16:40:38 +0200

    function that check every box

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
+let lastChecked;
+
 function handleCheck(e) {
-  console.log(e);
+  // check if they had the shift key down
+  // AND chech that they are checking it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    //go ahead and do what we please
+    // loop over every single checkbox
+    checkboxes.forEach((checkbox) => {
+      console.log(checkbox);
+    });
+  }
+  lastChecked = this;
 }
 
 checkboxes.forEach((checkbox) =>
